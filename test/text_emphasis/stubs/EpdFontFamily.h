05dd560709a670fe645973e3f58ffeb0c73f74a7
Beko6648 <beko6648@users.noreply.github.com>
2026-09-15 21:09:13 +0900

    style: text_emphasis テストスタブを fork clang-format で整形

--- test/text_emphasis/stubs/EpdFontFamily.h
+++ test/text_emphasis/stubs/EpdFontFamily.h
@@ -1,22 +1,34 @@
 #pragma once
+#include <array>
 #include <cstdint>
 #include <map>
-#include <array>
-struct EpdGlyph { uint8_t width=5, height=5; };
-struct EpdFontData { bool is2Bit=false; };
+struct EpdGlyph {
+  uint8_t width = 5, height = 5;
+};
+struct EpdFontData {
+  bool is2Bit = false;
+};
 class EpdFontFamily {
  public:
   enum Style : uint8_t {
-    REGULAR=0, BOLD=1, ITALIC=2, BOLD_ITALIC=3, UNDERLINE=4,
-    STRIKETHROUGH=8, SUPERSCRIPT=16, SUBSCRIPT=32, SCRIPT_MASK=SUPERSCRIPT | SUBSCRIPT
+    REGULAR = 0,
+    BOLD = 1,
+    ITALIC = 2,
+    BOLD_ITALIC = 3,
+    UNDERLINE = 4,
+    STRIKETHROUGH = 8,
+    SUPERSCRIPT = 16,
+    SUBSCRIPT = 32,
+    SCRIPT_MASK = SUPERSCRIPT | SUBSCRIPT
   };
   static constexpr uint8_t FONT_SELECT_MASK = BOLD | ITALIC;
-  std::map<uint32_t,EpdGlyph> glyphs;
-  mutable std::map<uint32_t,int> probes;
+  std::map<uint32_t, EpdGlyph> glyphs;
+  mutable std::map<uint32_t, int> probes;
   EpdFontData data;
   const EpdGlyph* getGlyphExact(uint32_t cp) const {
     probes[cp]++;
-    auto it=glyphs.find(cp); return it==glyphs.end()?nullptr:&it->second;
+    auto it = glyphs.find(cp);
+    return it == glyphs.end() ? nullptr : &it->second;
   }
   const EpdFontData* getData() const { return &data; }
 };
