9eb231ed476e7320e7e33ef56396315a1d1468cf
Y S <akiakane117@gmail.com>
2026-09-20 23:15:55 +0900

    feat: support landscape file browser

--- src/activities/home/FileBrowserActivity.h
+++ src/activities/home/FileBrowserActivity.h
@@ -74,4 +74,5 @@ class FileBrowserActivity final : public Activity {
   void onExit() override;
   void loop() override;
   void render(RenderLock&&) override;
+  bool supportsUiLandscape() const override { return true; }
 };
